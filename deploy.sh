7579abf6f4c85e4d4514acac588ad030f82394b7
Pavel Bedyaev <ddisel.13@gmail.com>
2020-12-16 15:01:46 +0300

    Fixed *.sh - run user check removed

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-if [ $(whoami) != "yc-user" ]
-  then echo "Please run as yc-user with sudoers permissions"
-  exit 99
-fi
-
 sudo apt install -y git
 cd ~
 git clone -b monolith https://github.com/express42/reddit.git
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-if [ $(whoami) != "yc-user" ]
-  then echo "Please run as yc-user with sudoers permissions"
-  exit 99
-fi
-
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
