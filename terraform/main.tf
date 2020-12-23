c49f0a7391aa4afd7f9bb3d26fd6bea115bffcdf
Pavel Bedyaev <ddisel.13@gmail.com>
2020-12-23 19:13:01 +0300

    before homework

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 provider "yandex" {
   version   = "~> 0.35.0"
-  service_account_key_file = "/home/pbedyaev/secrets/yc/terraform_key.json"
-  cloud_id  = "b1g7kiamf1mjb42t1lpa"
-  folder_id = "b1gke8b3gh5mjbpt1qsr"
-  zone      = "ru-central1-a"
+  service_account_key_file = var.service_account_key_file
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
@@ -16,16 +16,16 @@ resource "yandex_compute_instance" "app" {
   boot_disk {
     initialize_params {
       # Указать id образа созданного в предыдущем домашнем задании
-      image_id = "fd86gh35ock232282slf"
+      image_id = var.image_id
     }
   }
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9b1s7ga4doqd1feqrba"
+    subnet_id = var.subnet_id
     nat = true
   }
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/ubuntu.pub")}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
   connection {
     type = "ssh"
