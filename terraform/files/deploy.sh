c1024b0f5c07fdf45b0ec296d4190bab8ed5b58a
Pavel Bedyaev <ddisel.13@gmail.com>
2020-12-25 12:38:13 +0300

    Fix: sleep 30 added to deploy.sh: unattended upgrades.

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+echo "Sleeping 30s until unattended upgrades finished"
+sleep 30
+ps -ef | grep -i apt
 APP_DIR=${1:-$HOME}
 sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
